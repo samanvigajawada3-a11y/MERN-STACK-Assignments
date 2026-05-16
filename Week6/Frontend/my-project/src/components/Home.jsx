92e6b2323114a869c49e419335a876d3957eeff2
samanvigajawada3-a11y <samanvigajawada3@gmail.com>
2026-05-16 14:52:16 +0530

    Update Home.jsx

--- Week6/Frontend/my-project/src/components/Home.jsx
+++ Week6/Frontend/my-project/src/components/Home.jsx
@@ -5,27 +5,9 @@ import {useCounterStore} from "../store/CounterStore"
 
 function Home() {
 
-  console.log("Home")
-  // useCounterStore hook (to extract all the states)
-  // const {newCounter, incrementCounter, decrementCounter, resetCounter} = useCounterStore()
-  // To extract only few states
-  const newCounter = useCounterStore((state)=>state.newCounter)
-  const incrementCounter = useCounterStore((state)=>state.incrementCounter)
-  const decrementCounter = useCounterStore((state)=>state.decrementCounter)
-  const resetCounter = useCounterStore((state)=>state.resetCounter)
-
-  const {counter,changeCounter} = useContext(counterContextObj)
-
   return (
     <div className="text-center">
-      <h1 className="text-4xl mt-40">Counter : {counter}</h1>
-      <button onClick={changeCounter} className="bg-blue-400 p-5 rounded-2xl mt-5">Change</button>
-
-      <h1 className="text-4xl mt-40">New Counter : {newCounter}</h1>
-      <button onClick={incrementCounter} className="bg-blue-400 p-5 rounded-2xl mt-5">+</button>
-      <button onClick={decrementCounter} className="bg-blue-400 p-5 rounded-2xl mt-5">-</button>
-      <button onClick={resetCounter} className="bg-blue-400 p-5 rounded-2xl mt-5">reset</button>
-      <Test/>
+      <h1 className="text-7xl mt-60 text-gray-600 font-serif  animate-bounce">Welcome to the Home Page</h1>
     </div>
   )
 }
